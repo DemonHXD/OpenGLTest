d02d41a433f1ed48d8ba76d41e46534063771a32
hexiangda <2455188259@qq.com>
2021-04-04 15:47:59 +0800

    提交：项目可以直接编译并运行

--- OpenGLTest/src/main.cpp
+++ OpenGLTest/src/main.cpp
@@ -67,8 +67,8 @@ int main() {
 
 	glfwSetInputMode(windowIns, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glEnable(GL_DEPTH_TEST);
-	Shader ourShader("E:/openGL/openGL_C++_Pro/OpenGLTest/OpenGLTest/asset/vertexShader.vs",
-		"E:/openGL/openGL_C++_Pro/OpenGLTest/OpenGLTest/asset/fragmentShader.fs");
+	Shader ourShader("../OpenGLTest/asset/vertexShader.vs",
+		"../OpenGLTest/asset/fragmentShader.fs");
 	float vertices[] = {
 		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -151,7 +151,7 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	int width, height, nrChannels;
 	stbi_set_flip_vertically_on_load(true);
-	unsigned char *data = stbi_load("E:/openGL/openGL_C++_Pro/OpenGLTest/OpenGLTest/asset/container.jpg", &width, &height, &nrChannels, 0);
+	unsigned char *data = stbi_load("../OpenGLTest/asset/container.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -169,7 +169,7 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	data = stbi_load("E:/openGL/openGL_C++_Pro/OpenGLTest/OpenGLTest/asset/awesomeface.png", &width, &height, &nrChannels, 0);
+	data = stbi_load("../OpenGLTest/asset/awesomeface.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
